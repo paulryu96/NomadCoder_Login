14e944336029d318f1d404ad1257acd43222fae7
PaulRyu <40940136+paulryu96@users.noreply.github.com>
2021-08-13 14:44:09 +0900

    08.13 added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const loginForm = document.querySelector('#container')
 const loginInput = loginForm.querySelector('input')
 const showUserName = document.querySelector('#showUserName')
+
 const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 function onLoginSubmit(event){
   event.preventDefault()
   loginForm.classList.add(HIDDEN_CLASSNAME)
   const userName = loginInput.value
+  localStorage.setItem(USERNAME_KEY, userName)
   showUserName.innerText = `Hello ${userName}`
   showUserName.classList.remove(HIDDEN_CLASSNAME)
 
 }
 
-
 loginForm.addEventListener('submit', onLoginSubmit)
+
+const savedUserName = localStorage.getItem(USERNAME_KEY)
+
+if (savedUserName === null){
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+} else {
+  const userName = loginInput.value
+  showUserName.innerText = `Hello ${savedUserName}`
+  showUserName.classList.remove(HIDDEN_CLASSNAME)
+
+}
